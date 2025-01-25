5216dfdc82b8709a7be43a7a78dd1eb384c65125
Saanvi Sharma <saanvisharma004@gmail.com>
2025-01-25 13:47:39 +0000

    Save progress: 1290.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -68,8 +68,9 @@ RENT_MENU() {
           # get new customer name
           echo -e "\nWhat's your name?"
           read CUSTOMER_NAME
-
           # insert new customer
+                    INSERT_CUSTOMER_RESULT=$($PSQL"INSERT INTO customers(phone, name) values('$PHONE_NUMBER','$CUSTOMER_NAME')")
+
 
         fi
       fi
